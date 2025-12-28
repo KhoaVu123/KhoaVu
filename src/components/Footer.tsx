d33375adddad7f8918fe39873855bb61faf9065d
Portfolio Owner <you@example.com>
2025-12-28 18:56:03 +0700

    init

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
     <footer>
       <div>
         <a href="https://github.com/KhoaVu123" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://www.linkedin.com/in/khoa-nguy%E1%BB%85n-v%C5%A9-17523b3a2/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
       <p>A portfolio designed & built by <a href="https://github.com/KhoaVu123/react-portfolio-template" target="_blank" rel="noreferrer">Nguyen Vu Khoa</a> with 💜</p>
     </footer>
